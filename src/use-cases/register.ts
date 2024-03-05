e69ad59b072cf5e3529608e3e81012f1bb586241
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-04 21:38:16 -0300

    feat: implementando a inversão de dependencia

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma';
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
 import { hash } from 'bcryptjs';
 
 interface RegisterUseCaseRequest {
@@ -8,28 +7,25 @@ interface RegisterUseCaseRequest {
 	password: string;
 }
 
-export async function registerUseCase({
-	name,
-	email,
-	password,
-}: RegisterUseCaseRequest) {
-	const password_hash = await hash(password, 10);
+export class RegisterUseCase {
+	constructor(private usersRepository: any) {}
+	async execute({ name, email, password }: RegisterUseCaseRequest) {
+		const password_hash = await hash(password, 10);
 
-	const userWithSameEmail = await prisma.user.findUnique({
-		where: {
+		const userWithSameEmail = await prisma.user.findUnique({
+			where: {
+				email,
+			},
+		});
+
+		if (userWithSameEmail) {
+			throw new Error('E-mail already exists.');
+		}
+
+		await this.usersRepository.create({
+			name,
 			email,
-		},
-	});
-
-	if (userWithSameEmail) {
-		throw new Error('E-mail already exists.');
+			password_hash,
+		});
 	}
-
-	const prismaUsersRepository = new PrismaUsersRepository();
-
-	await prismaUsersRepository.create({
-		name,
-		email,
-		password_hash,
-	});
 }
